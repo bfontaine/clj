561d3e298d1dffa62998b0a6bcbaafae693ffd26
Baptiste Fontaine <b@ptistefontaine.fr>
2020-07-14 16:16:44 +0200

    Release 0.1.4

--- clj/__init__.py
+++ clj/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from clj.seqs import (
     butlast, concat, cons, count, cycle, distinct, dorun, drop, drop_last, drop_while, empty, every, ffirst, filter,
