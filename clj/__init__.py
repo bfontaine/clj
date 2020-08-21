a4c07aa3e6fadaa8ccc4b90a4ffca077b1baf44f
Jaihindh Reddy <jaihindhreddy@gmail.com>
2020-08-21 21:43:51 +0530

    add dedupe (#3)
    Co-authored-by: Jaihindh Reddy <jaihindh.reddy@belong.co>

--- clj/__init__.py
+++ clj/__init__.py
@@ -3,7 +3,7 @@
 __version__ = "0.1.4"
 
 from clj.seqs import (
-    butlast, concat, cons, count, cycle, distinct, dorun, drop, drop_last, drop_while, empty, every, ffirst, filter,
+    butlast, concat, cons, count, cycle, dedupe, distinct, dorun, drop, drop_last, drop_while, empty, every, ffirst, filter,
     first, flatten, group_by, interleave, interpose, is_seq, iterate, keep, keep_indexed, last, map_indexed, map,
     mapcat, nfirst, not_any, not_every, nth, range, reductions, remove, repeat, repeatedly, replace, rest, reverse,
     second, shuffle, some, split_at, split_with, take, take_nth, take_while, tree_seq, zipmap)
