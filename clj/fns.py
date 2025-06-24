d55338336580d0767d2c06c996d970caeeaecf75
Baptiste Fontaine <baptiste@bixoto.com>
2025-06-24 17:18:42 +0200

    fns: add explicit types

--- clj/fns.py
+++ clj/fns.py
@@ -1,9 +1,12 @@
 # -*- coding: UTF-8 -*-
 
 # See http://clojure.org/reference/other_functions
-from typing import TypeVar, Union, Callable, Hashable
+from typing import TypeVar, Callable, Hashable, Any, ParamSpec
 
+X = TypeVar('X')
 T = TypeVar('T')
+Params = ParamSpec('Params')
+Number = TypeVar('Number', int, float)
 
 
 def identity(x: T) -> T:
@@ -13,35 +16,36 @@ def identity(x: T) -> T:
     return x
 
 
-def inc(x: Union[int, float]):
+def inc(x: Number) -> Number:
     """
     Returns a number one greater than num.
     """
     return x + 1
 
 
-def dec(x: Union[int, float]):
+def dec(x: Number) -> Number:
     """
     Returns a number one less than num.
     """
     return x - 1
 
 
-def is_even(x: int):
+def is_even(x: int) -> bool:
     """
     Return ``True`` if ``x`` is an even number.
     """
     return bool(~x & 1)
 
 
-def is_odd(x: int):
+def is_odd(x: int) -> bool:
     """
     Return ``True`` if ``x`` is an odd number.
     """
     return bool(x & 1)
 
 
-def comp(*fns: Callable) -> Callable:
+# TODO: better typing
+def comp(*fns: Callable[..., Any]) -> Callable[..., Any]:
     """
     Takes a set of functions and returns a function that is the composition of
     those functions. The returned function takes a variable number of args,
@@ -51,7 +55,7 @@ def comp(*fns: Callable) -> Callable:
     if not fns:
         return constantly(None)
 
-    def _comp(*args, **kw):
+    def _comp(*args: Any, **kw: Any) -> Any:
         res = fns[-1](*args, **kw)
 
         for i in range(1, len(fns)):
@@ -62,14 +66,14 @@ def comp(*fns: Callable) -> Callable:
     return _comp
 
 
-def complement(f: Callable) -> Callable[..., bool]:
+def complement(f: Callable[Params, Any]) -> Callable[Params, bool]:
     """
     Takes a function ``f`` and returns a function that takes the same arguments
     as ``f``, has the same effects, if any, and returns the opposite truth
     value.
     """
 
-    def _f(*args, **kw):
+    def _f(*args: Any, **kw: Any) -> bool:
         return not f(*args, **kw)
 
     return _f
@@ -80,31 +84,31 @@ def constantly(x: T) -> Callable[..., T]:
     Returns a function that takes any number of arguments and returns ``x``.
     """
 
-    def _fn(*_args, **_kw):
+    def _fn(*_args: Any, **_kw: Any) -> T:
         return x
 
     return _fn
 
 
-def juxt(*fns) -> Callable[..., list]:
+def juxt(*fns: Callable[Params, T]) -> Callable[Params, list[T]]:
     """
     Takes a set of functions and returns a function that is the juxtaposition
     of those functions. The returned function takes a variable number of
     arguments, and returns a list containing the result of applying each
     function to the arguments (left-to-right).
 
-        juxt(a, b, c)(x) # => [a(x), b(x), c(x)]
+        juxt(f, g, h)(x) # => [f(x), g(x), h(x)]
     """
 
     # Note we accept zero argument while Clojure wants at least one.
 
-    def _fn(*args, **kw):
+    def _fn(*args: Any, **kw: Any) -> list[T]:
         return [f(*args, **kw) for f in fns]
 
     return _fn
 
 
-def is_distinct(*args: Hashable):
+def is_distinct(*args: Hashable) -> bool:
     s = set()
     for arg in args:
         if arg in s:
